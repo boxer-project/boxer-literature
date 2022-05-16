49bc5c6c692aaac4878f2970bbc7269b974b1270
Antranig Basman <antranig.basman@colorado.edu>
2022-05-16 10:29:10 +0100

    Moving to "docs" system for publishing GitHub pages

--- .eleventy.js
+++ .eleventy.js
@@ -21,7 +21,7 @@ module.exports = function (eleventyConfig) {
             input: "src",
             includes: "_includes",
             data: "_data",
-            output: "dist"
+            output: "docs"
         }
     };
 };
